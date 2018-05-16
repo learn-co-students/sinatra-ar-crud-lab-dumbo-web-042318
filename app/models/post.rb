70ba9d886e759f75e8a85f3a44167c54c5338a69
jcr2125 <email>
2018-05-16 11:55:46 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,19 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+
+  # attr_accessor :name, :content
+  #
+  # @@all = []
+  #
+  # def initialize(args)
+  #   @name = args[:name]
+  #   @content = args[:content]
+  #   self.class.all << self
+  # end
+  #
+  # def self.all
+  #   @@all
+  # end
+
 end
